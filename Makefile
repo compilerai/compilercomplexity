756726615f2d55186a8fbf347b81a91a8cbb6d22
Sorav Bansal <sbansal@iitd.ac.in>
2019-12-23 17:23:03 +0530

    works; but too slow

--- Makefile
+++ Makefile
@@ -1,13 +1,20 @@
 GCC_REPO=https://github.com/gcc-mirror/gcc
 LLVM_REPO=https://github.com/llvm-mirror/llvm
+EVERY_NTH_COMMIT=100
 
 measure::
-	perl plot.pl gcc
-	perl plot.pl llvm
+	rm -f commitcount
+	perl plot.pl gcc commitcount $(EVERY_NTH_COMMIT)
+	rm -f commitcount
+	perl plot.pl llvm commitcount $(EVERY_NTH_COMMIT)
+	rm -f commitcount
 
 plot::
-	git -C gcc rebase --exec "cd ../ && perl plot.pl gcc && cd gcc" 3cf0d8938a953e && cd ..
-	git -C llvm rebase --exec "cd ../ && perl plot.pl llvm && cd llvm" 8d0afd3d32d1d && cd ..
+	rm -f commitcount
+	git -C gcc rebase --exec "cd ../ && perl plot.pl gcc commitcount $(EVERY_NTH_COMMIT) && cd gcc" 3cf0d8938a953e && cd ..
+	rm -f commitcount
+	git -C llvm rebase --exec "cd ../ && perl plot.pl llvm commitcount $(EVERY_NTH_COMMIT) && cd llvm" 8d0afd3d32d1d && cd ..
+	rm -f commitcount
 
 clone_repos::
 	rm -rf gcc llvm
--- plot.pl
+++ plot.pl
@@ -3,13 +3,21 @@
 use strict;
 use warnings;
 
-$#ARGV >= 0 or die "Usage: plot.pl <gcc|llvm>";
+$#ARGV >= 1 or die "Usage: plot.pl <gcc|llvm> <commitcount-filename> [<every-nth-commit>]";
 my $compiler = $ARGV[0];
+my $commitcount_filename = $ARGV[1];
+my $every_nth_commit = 1;
+if ($#ARGV >= 2) {
+  $every_nth_commit = int($ARGV[2]);
+}
 
-my $date = get_commit_date($compiler);
-my $tot = count_total($compiler);
-#print "Number of lines in $compiler: $tot\n";
-print "$compiler\t$date\t$tot\n";
+my $need_to_commit = identify_need_to_commit($compiler, $commitcount_filename, $every_nth_commit);
+if ($need_to_commit) {
+  my $date = get_commit_date($compiler);
+  my $tot = count_total($compiler);
+  #print "Number of lines in $compiler: $tot\n";
+  print "\t$compiler\t$date\t$tot\n";
+}
 
 sub count_total {
   my $compiler = shift;
@@ -53,4 +61,36 @@ sub get_commit_date {
   my $date = `cd $gitrep && git show -s --format=%ci && cd ..`;
   chomp($date);
   return $date;
+};
+
+sub identify_need_to_commit {
+  my $compiler = shift;
+  my $commitcount_filename = shift;
+  my $every_nth_commit = shift;
+
+  if (not (-e $commitcount_filename)) {
+    open(my $outfp, ">$commitcount_filename");
+    print $outfp "LAST COMMITTED: 0\n";
+    print $outfp "LAST SEEN: 0\n";
+    close($outfp);
+    return 1;
+  }
+  open(my $infp, "<$commitcount_filename");
+  my $line1 = <$infp>;
+  my $line2 = <$infp>;
+  close($infp);
+  $line1 =~ /LAST COMMITTED: (\d*)$/ or die;
+  my $last_committed = int($1);
+  $line2 =~ /LAST SEEN: (\d*)$/ or die;
+  my $last_seen = int($1);
+  my $cur_seen = $last_seen + 1;
+  my $cur_committed = $last_committed;
+  if ($cur_seen >= $last_committed + $every_nth_commit) {
+    $cur_committed = $cur_seen;
+  }
+  open(my $outfp, ">$commitcount_filename");
+  print $outfp "LAST COMMITTED: $cur_committed\n";
+  print $outfp "LAST SEEN: $cur_seen\n";
+  close($outfp);
+  return $cur_committed > $last_committed;
 };
